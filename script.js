a4d653e1324cb1110dc81a8775641c3f7f6084e3
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2025-09-12 20:30:39 +0530

    8th project

---
+++ script.js
@@ -0,0 +1,30 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;// password ki length ke liye
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|{}[]></-="; 
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+// function that create an random password
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+         password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passwordBox.value = password;
+}
+
+// copy button ke liye
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
